f435f1010d3a0c5dd836372384544fa06b29269f
mrgavinconway <mrgavinconway@gmail.com>
2026-06-11 18:32:46 +0100

    Add multiACE integration constants

---
+++ custom_components/multiace/const.py
@@ -0,0 +1,23 @@
+"""Constants for the multiACE integration."""
+
+from __future__ import annotations
+
+from logging import Logger, getLogger
+
+DOMAIN = "multiace"
+LOGGER: Logger = getLogger(__package__)
+
+CONF_BASE_URL = "base_url"
+CONF_DRYER_TEMP = "dryer_temp"
+CONF_DRYER_DURATION = "dryer_duration"
+
+DEFAULT_BASE_URL = "http://u1.lan/multiace/"
+DEFAULT_DRYER_TEMP = 50
+DEFAULT_DRYER_DURATION = 240
+
+MAX_ACE_COUNT = 4
+SLOT_COUNT = 4
+SCAN_INTERVAL_SECONDS = 15
+
+ATTR_ACE = "ace"
+ATTR_SLOT = "slot"
