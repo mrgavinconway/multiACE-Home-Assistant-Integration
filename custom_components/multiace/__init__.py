ba2005c5896cbf9b1d464095ec2d2d64de4552a8
mrgavinconway <mrgavinconway@gmail.com>
2026-06-11 18:33:45 +0100

    Add multiACE setup entry

---
+++ custom_components/multiace/__init__.py
@@ -0,0 +1,33 @@
+"""multiACE integration."""
+
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+from .api import MultiAceApi
+from .const import CONF_BASE_URL, DOMAIN
+from .coordinator import MultiAceCoordinator
+
+PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.SWITCH]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up multiACE from a config entry."""
+    api = MultiAceApi(async_get_clientsession(hass), entry.data[CONF_BASE_URL])
+    coordinator = MultiAceCoordinator(hass, api)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
