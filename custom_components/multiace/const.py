5263393db80f2d2082cc9131c81005744013f14f
mrgavinconway <mrgavinconway@gmail.com>
2026-06-11 18:55:31 +0100

    Add per-ACE dryer option keys

--- custom_components/multiace/const.py
+++ custom_components/multiace/const.py
@@ -10,6 +10,8 @@ LOGGER: Logger = getLogger(__package__)
 CONF_BASE_URL = "base_url"
 CONF_DRYER_TEMP = "dryer_temp"
 CONF_DRYER_DURATION = "dryer_duration"
+CONF_ACE_DRYER_TEMP = "ace_dryer_temp"
+CONF_ACE_DRYER_DURATION = "ace_dryer_duration"
 
 DEFAULT_BASE_URL = "http://yourprinter/multiace/"
 DEFAULT_DRYER_TEMP = 50
@@ -21,3 +23,13 @@ SCAN_INTERVAL_SECONDS = 15
 
 ATTR_ACE = "ace"
 ATTR_SLOT = "slot"
+
+
+def ace_dryer_duration_option(ace_index: int) -> str:
+    """Return the option key for one ACE dryer duration."""
+    return f"{CONF_ACE_DRYER_DURATION}_{ace_index}"
+
+
+def ace_dryer_temp_option(ace_index: int) -> str:
+    """Return the option key for one ACE dryer temperature."""
+    return f"{CONF_ACE_DRYER_TEMP}_{ace_index}"
