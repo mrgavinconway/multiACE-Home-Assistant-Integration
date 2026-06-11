652398462c53d9c1dd0f204b1158e61c6c620b47
mrgavinconway <mrgavinconway@gmail.com>
2026-06-11 18:39:29 +0100

    Use generic printer URL default

--- custom_components/multiace/const.py
+++ custom_components/multiace/const.py
@@ -11,7 +11,7 @@ CONF_BASE_URL = "base_url"
 CONF_DRYER_TEMP = "dryer_temp"
 CONF_DRYER_DURATION = "dryer_duration"
 
-DEFAULT_BASE_URL = "http://u1.lan/multiace/"
+DEFAULT_BASE_URL = "http://yourprinter/multiace/"
 DEFAULT_DRYER_TEMP = 50
 DEFAULT_DRYER_DURATION = 240
 
